f27e34e34484ce3403a1f23305fd6de1adcb49e0
Hema <hema.victory@gmail.com>
2022-08-23 13:39:44 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test // if we dont want to run, we can comment like this //@Test 
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
